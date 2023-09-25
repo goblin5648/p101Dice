941f69ffd87c5f39cdd55cd0f38bb9353bcce35c
goblin5648 <110573365+goblin5648@users.noreply.github.com>
2023-09-24 18:04:13 -0700

    Add files via upload

---
+++ p101.py
@@ -0,0 +1,42 @@
+import random
+response='y'
+while response == 'y':
+    no = random.randint(1,6)
+    if no == 1:
+        print('-----')
+        print('     ')
+        print('  0  ')
+        print('     ')
+        print('-----')
+    elif no == 2:
+        print('-----')
+        print('  0  ')
+        print('     ')
+        print('  0  ')
+        print('-----')
+    elif no == 3:
+        print('-----')
+        print('  0  ')
+        print('  0  ')
+        print('  0  ')
+        print('-----')
+    elif no == 4:
+        print('-----')
+        print(' 0 0 ')
+        print('     ')
+        print(' 0 0 ')
+        print('-----')
+    elif no == 5:
+        print('-----')
+        print(' 0 0 ')
+        print('  0  ')
+        print(' 0 0 ')
+        print('-----')
+    elif no == 6:
+        print('-----')
+        print(' 0 0 ')
+        print(' 0 0 ')
+        print(' 0 0 ')
+        print('-----')
+    response = input ('press y to continue and n to stop ')
+    print('\n')
